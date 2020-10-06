9c7cb6e181bcd05ab6d9b10eb1b3288c77ff3e8f
Anny Ramos <annynk.ramos@gmail.com>
2020-10-06 16:36:42 -0400

    Login

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './Login.scss';
+
+import Logo from '../../images/logo.png'
 //redux
 import {useDispatch,useSelector} from 'react-redux';
 import {LoginAction} from '../../Redux/Dusk/loginreducer';
@@ -9,9 +11,36 @@ import {LoginAction} from '../../Redux/Dusk/loginreducer';
 function Login(){
   const disparador="";
   return (
-        <div>
-          <button onClick={()=>{}}>hola</button>
-        </div>
+    <div>
+      <article className="border-login card-dark card-Lg">
+        {/*Contenedor de la imagen*/}
+        <img src={Logo} />
+
+        {/*Contenido*/}
+        <form action="#" className="s-mb-0">
+          <div className="form-group">
+            <label>USUARIO</label>
+            <input type="email" name="email" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>CONTRASE'A</label>
+            <input name="password" className="form-control" type="password" />
+          </div>
+          <div className="form-group">
+            <label className="custom-radio-checkbox">
+              {/* Input oculto */}
+              <input className="custom-radio-checkbox__input" type="checkbox" name="recuerdame" defaultValue="recuerdame" />
+              {/* Imagen en sustitucion */}
+              <span className="custom-radio-checkbox__show custom-radio-checkbox__show--checkbox" />
+              {/* Texto */}
+              <span className="custom-radio-checkbox__text">RECUERDAME</span>
+            </label>
+          </div>
+          <button className="bttn">AUTENTICAR</button>
+        </form>
+      </article>
+
+    </div>
   );
 }
 
