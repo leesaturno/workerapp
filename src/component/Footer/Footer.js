b8d4d09e80108685c89234b465b4c1dde986aaa4
Anny Ramos <annynk.ramos@gmail.com>
2020-10-15 22:22:14 -0400

    Footer corto

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
-
 import './Footer.scss';
 
 function Footer() {
   return (
     <div>
         <footer>
-            <p className="text-ups">Copyright &copy; 2020 T-work Social Selling. WorkerApp y T-work Social Selling son marcas registradas de T-work Chile SpA.</p>
+            <p className="text-ups">T-work Copyright  &copy; 2020. All rights reserved</p>
         </footer>
     </div>
   );
